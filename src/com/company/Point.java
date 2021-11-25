27f221734a84f786df7d275872e11dd6053f61fa
brittmoni <brittfranklin92@gmail.com>
2021-11-24 19:53:52 -0500

    Adding methods

--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -4,4 +4,40 @@ public class Point {
 
   private int x;
   private int y;
+
+  public Point() {
+  }
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public double distance() {
+    return 1;
+  }
+
+  public double distance(int x, int y) {
+    return 1;
+  }
+
+  public double distance(Point point) {
+    return 1;
+  }
 }
